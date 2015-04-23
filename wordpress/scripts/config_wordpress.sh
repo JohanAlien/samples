1adc60e3a82dd31a4cfd834a3e12cea6f9924649
Mohamed Hadrouj <hadrouj@gmail.com>
2015-04-23 18:49:42 +0200

    changes chmod 777 to more restricted rights. No need to restart apache after installing Wordpress

--- wordpress/scripts/config_wordpress.sh
+++ wordpress/scripts/config_wordpress.sh
@@ -10,7 +10,4 @@ fi
 sudo rm -rf $DOC_ROOT/$CONTEXT_PATH/*
 sudo mv -f /opt/wordpress/wordpress/* $DOC_ROOT/$CONTEXT_PATH
 sudo chown -R www-data:www-data $DOC_ROOT/$CONTEXT_PATH
-sudo chmod 777 -R $DOC_ROOT/$CONTEXT_PATH
-
-echo "End of Wordpress install, restart apache2 to charge all modules"
-sudo /etc/init.d/apache2 restart
+sudo chmod 644 -R $DOC_ROOT/$CONTEXT_PATH
