61bfec7c1d28912704b9c5d42f89ccc2df0be526
Mohamed Hadrouj <hadrouj@gmail.com>
2015-04-24 10:32:51 +0200

    accept automatically apt daemon install and php5-mysql

--- wordpress/scripts/install_php_module.sh
+++ wordpress/scripts/install_php_module.sh
@@ -6,9 +6,9 @@ echo "Install PHP module for Mysql..."
 sudo apt-get update || (sleep 15; sudo apt-get update || exit ${1})
 
 echo "Checking if Aptdaemon is installed..."
-command aptdcon > /dev/null 2>&1 || { echo >&2 "Aptdaemon is required but it's not installed."; sudo apt-get install aptdaemon; }
+command aptdcon > /dev/null 2>&1 || { echo >&2 "Aptdaemon is required but it's not installed."; sudo apt-get -y install aptdaemon; }
 
-yes | sudo aptdcon --install php5-mysql || exit ${1}
+yes|sudo aptdcon --install php5-mysql || exit ${1}
 
 echo "restart apache2 to launch php5-mysql"
 sudo service apache2 restart
